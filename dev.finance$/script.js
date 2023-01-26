da0c6831f1aac5966961dbc09ea6534565ad5110
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2023-01-26 13:13:27 -0300

    dev.finance$

---
+++ dev.finance$/script.js
@@ -0,0 +1,11 @@
+var modal={
+    open(){
+        //Abrir o modal para isso tenho que 
+        //add a class active ao var modal em js
+        alert('abrir o modal')
+    },
+    close(){
+        //Fechar o modal para isso tenho que remover 
+        // a class active do var modal em js 
+    }
+}
